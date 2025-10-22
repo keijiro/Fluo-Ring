649cb2074d82efc9a0f2e52f37006ad24ccdaa1e
Keijiro Takahashi <keijiro@gmail.com>
2025-10-22 10:49:55 +0900

    Shell script for code signing

---
+++ Misc/sign.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+APP_NAME="Fluo"
+APP_DIR=${APP_NAME}.app
+APP_ZIP=${APP_NAME}.zip
+
+chmod -R a+xr $APP_DIR
+
+codesign --deep --force --verify --verbose --timestamp --options runtime \
+  --entitlements "Fluo.entitlements" \
+  --sign "Developer ID Application: Keijiro Takahashi (N6V3488B33)" \
+  $APP_DIR
+
+ditto -c -k --sequesterRsrc --keepParent $APP_DIR $APP_ZIP
+
+xcrun notarytool submit $APP_ZIP --keychain-profile NotaryProfile --wait
+xcrun stapler staple $APP_DIR
+
+spctl --assess --type execute -vv $APP_DIR
+codesign -dv --verbose=4 $APP_DIR
